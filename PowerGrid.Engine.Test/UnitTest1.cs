8dad1b66a2aa97692fb2b4ed68e164ff01d5e288
Patrick Huizinga <Patrick.Huizinga@gmail.com>
2020-10-24 13:07:56 +0200

    Initial solution and project structure.

---
+++ PowerGrid.Engine.Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace PowerGrid.Engine.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ PowerGrid.Engine/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PowerGrid.Engine
+{
+    public class Class1
+    {
+    }
+}
